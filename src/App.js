b0d79953cbf100bfd85c1e7e8057ecee7c0f1cd8
Ignatius <musondaignatius@gmail.com>
2022-12-04 17:38:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {useEffect, useState} from 'react'
+import SideBar from './Components/SideBar/SideBar';
+import {Routes,Route,} from "react-router-dom"
+import Overview from './Components/Overview/Overview';
+import Settings from './Components/Settings/Settings';
+import Marketplace from './Pages/Marketplace/Marketplace';
+import KYC from './Pages/KYC/KYC';
+import {Home} from "../src/Components/Home"
+import {NavBar} from "../src/Components/Navbar"
+// import {MarketPlace} from "./components/marketplace"
+// import{Marketplace} from "../src/Components/Home"
+// import {About} from "./components/about"
+
+// import {Learn} from "./learn"
+// import {Learn} from "../src/Components/"
+// import "./App.css"
+
+// import {APIURL,
+//   LENS_HUB_CONTRACT_ADDRESS,
+//   queryRecommendedPublications,
+//   queryRecommendedPublications,
+//   urlClient
+// } from "./Queries"; 
+
+// import LENSHUB from "./Lenshub"
+// import { ethers } from 'ethers';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App"> 
+          
+    //       HELLO
+    // </div>  
+    <> 
+     {/* <div className='wholeDash'>
+        <SideBar/> */}
+      
+          
+          <Routes>
+               <Route index path="overview" element={<Overview />}>
+                     
+              </Route> 
+
+            <Route path="about" element={<Settings />}>
+               
+            </Route>
+            <Route path="/marketplace" element={<Marketplace />}>
+               
+            </Route>
+            <Route path="/" element={<KYC />}>
+               
+            </Route>
+                 
+          </Routes> 
+          {/* </div> */}
+
+    </>
   );
 }
 
---
+++ src/Components/SideBar/SideBar.js
@@ -0,0 +1,28 @@
+
+import "./SideBar.css"
+
+function SideBar(){  
+
+    return( 
+
+        <>
+             <div className="SideBarCover">  
+                 <div className="Logo"><h1>BloKTrack</h1></div>
+                 <div className="SideList">
+                    <ul>
+                        <li><p>Overview</p></li>
+                        <li><p>Settings</p></li>
+                        <li><p>Support</p></li>
+                     
+                    </ul>
+                 </div>
+
+
+            </div>
+                
+        </>
+    )
+
+}
+
+export default SideBar;
